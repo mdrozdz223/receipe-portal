f06c4e53bc78f4af9c78d5edacb25e3193015c0c
Mikołaj Dróżdż <mikodrozdz13@gmail.com>
2026-06-16 20:16:44 +0200

    Initial commit

---
+++ src/main/java/com/example/receipeportal/ReceipePortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.receipeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceipePortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceipePortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/receipeportal/ReceipePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.receipeportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceipePortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
